644b6a267deb849c94f9f94a5a0c8accf0589840
Sean Mackesey <s.mackesey@gmail.com>
2020-09-08 16:00:24 -0700

    placeholders for other pages

--- web/src/pages/NotebooksPage/NotebooksPage.js
+++ web/src/pages/NotebooksPage/NotebooksPage.js
@@ -1,15 +1,11 @@
 import { Link, routes } from '@redwoodjs/router'
+import MasterLayout from 'src/layouts/MasterLayout/MasterLayout'
 
 const NotebooksPage = () => {
   return (
-    <>
-      <h1>NotebooksPage</h1>
-      <p>Find me in "./web/src/pages/NotebooksPage/NotebooksPage.js"</p>
-      <p>
-        My default route is named "notebooks", link to me with `
-        <Link to={routes.notebooks()}>Notebooks</Link>`
-      </p>
-    </>
+    <MasterLayout>
+      TODO
+    </MasterLayout>
   )
 }
 
--- web/src/pages/TimelinePage/TimelinePage.js
+++ web/src/pages/TimelinePage/TimelinePage.js
@@ -1,15 +1,11 @@
 import { Link, routes } from '@redwoodjs/router'
+import MasterLayout from 'src/layouts/MasterLayout/MasterLayout'
 
 const TimelinePage = () => {
   return (
-    <>
-      <h1>TimelinePage</h1>
-      <p>Find me in "./web/src/pages/TimelinePage/TimelinePage.js"</p>
-      <p>
-        My default route is named "timeline", link to me with `
-        <Link to={routes.timeline()}>Timeline</Link>`
-      </p>
-    </>
+    <MasterLayout>
+      TODO
+    </MasterLayout>
   )
 }
 
